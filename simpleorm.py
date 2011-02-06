da595b54adf56a47250bbe2f84bb3f4658c9d1e7
Jeffrey Melloy <jmelloy@gmail.com>
2011-02-06 14:50:11 -0800

    Cleaned up interface a bit; added JSON return.

--- simpleorm.py
+++ simpleorm.py
@@ -1,44 +1,123 @@
-from datetime import datetime
+import datetime
+import json
+import decimal
+
+class JSONCustomEncoder(json.JSONEncoder):
+    """Encodes dates with ISO formatting"""
+    def default(self, obj):
+        if isinstance(obj, (datetime.date, datetime.datetime)):
+            return obj.isoformat(' ')
+        else:
+            return json.JSONEncoder.default(self, obj)
 
 class Query():
     def __init__(self, sql):
         self.sql = sql
-    
+        self.start_time = None
+        self.end_time = None
+        self.error = None
+        
     def __str__(self):
         return self.sql
 
     def exec_select(self, conn, return_type ='dict'):
-        self.start_time = datetime.now()
-        curs = conn.cursor()
-        curs.execute(self.sql)
-        self.end_time = datetime.now()
-        
-        cols = [x[0].lower() for x in curs.description]
-        while(True):
-            results = curs.fetchmany(25)
-            if not results:
-                break
-            for r in results:
-                if return_type  == 'dict':
-                    yield dict(zip(cols,r))
-                if return_type == 'rset':
-                    yield r
-            
-    def exec_update(self, conn):
-        self.start_time = datetime.now()
-        curs = conn.cursor()
-        curs.execute(self.sql)
-        self.end_time = datetime.now()
-        
-        return curs.rowcount
+        """Returns the value of the SQL as a generator as either a dict or result set."""
+        self.start_time = datetime.datetime.now()
+        try:
+            curs = conn.cursor()
+            curs.execute(self.sql)
+            self.end_time = datetime.datetime.now()
+    
+            cols = [x[0].lower() for x in curs.description]
+            while(True):
+                results = curs.fetchmany(25)
+                if not results:
+                    break
+                for r in results:
+                    if return_type  == 'dict':
+                        yield dict(zip(cols,r))
+                    if return_type == 'rset':
+                        yield r
+        except Exception, e:
+            self.end_time = datetime.datetime.now()
+            self.error_text = str(e)
+            raise e
 
+    def exec_select_json(self, conn):
+        """ Turns a result set into a JSON object.
+        Output looks like this:
+        {
+            "rows": [[17, "Cally's War (Posleen War Series #5)", "John Ringo, Julie Cochrane", "141652052X", "Baen"], 
+                    [19, "Last Chance to See", "Douglas Adams, Mark Carwardine", "0345371984", "Ballantine Books"], 
+                    [20, "Dirk Gently's Holistic Detective Agency", "Douglas Adams", "0671746723", "Pocket"], 
+                    [21, "The Long Dark Tea-Time of the Soul", "Douglas Adams", "0671742515", "Pocket Books"], 
+                    [26, "So Long, and Thanks for All the Fish", "Douglas Adams", "0345479963", "Del Rey"]],
+            "total_rows": 5,
+            "run_time": "0.002321",
+            "run_date": "2010-11-06T15:59:21.810127",
+            "sql": "select id, title, author, asin, publisher from library_books limit 5",
+            "columns": ["id", "title", "author", "asin", "publisher"]
+        }
+        """
+        
+        curs = conn.cursor()
+        self.start_time = datetime.datetime.now()
+        query = {"sql":self.sql, "run_date":self.start_time}
+        try:
+            curs.execute(self.sql)
+            self.end_time = datetime.datetime.now()
+        
+            columns = [i[0] for i in curs.description]
+            rows = []
+
+            row = curs.fetchone()
+            while row:
+                rows.append(row)
+                row = curs.fetchone()
+
+            query["columns"] = columns
+            query["rows"] = rows
+            query["run_date"] = end_time
+            query["run_time"] = self.delta_to_seconds(end_time - start_time)
+            query["total_rows"] = curs.rowcount
+            
+            
+            x = json.dumps(query,cls=JSONCustomEncoder)
+
+            return x
+        except Exception, e:
+            self.error = str(e)
+            self.end_time = datetime.datetime.now()
+
+            raise e
+            
+
+    def exec_update(self, conn):
+        self.start_time = datetime.datetime.now()
+        try:
+            curs = conn.cursor()
+            curs.execute(self.sql)
+            self.end_time = datetime.datetime.now()
+        
+            return curs.rowcount
+        except Exception, e:
+            self.end_time = datetime.datetime.now()
+            self.error = str(e)
+            raise e
+            
     @staticmethod
     def bind(a):
         return str(a).replace("'", "''")
+    
+    @staticmethod
+    def delta_to_seconds(dt):
+        d = decimal.Decimal(dt.days * 24 * 3600)
+        d = d + dt.seconds
+        d = d + (decimal.Decimal(dt.microseconds) / 1000000)
 
-
-            
-class Connection():
+        return str(d)
+    
+class DBConnection():
             
     def __init__(self, conn):
         self.conn = conn
@@ -47,7 +126,7 @@ class Connection():
     def __str__(self):
         return str(self.conn)
     
-    def select(self, from_clause, where = None, order = None, select_list = None):
+    def select(self, from_clause, where = None, order = None, select_list = None, return_type='dict'):
         select_clause =  " * "
         if select_list:
             select_clause = ", ".join(select_list)
@@ -60,17 +139,24 @@ class Connection():
         
         q = Query(query)
         self.query_list.append(q)
-        return q.exec_select(self.conn)
+        
+        if return_type == 'json':
+            return q.exec_select_json(self.conn)
+        else:
+            return q.exec_select(self.conn, return_type)
 
-    def select_sql(self, sql):
+    def select_sql(self, sql, return_type = 'dict'):
         q = query(sql)
         self.query_list.append(q)
-        return q.exec_select(self.conn)
+        if return_type == 'json':
+            return q.exec_select_json(self.conn)
+        else:
+            return q.exec_select(self.conn, return_type)
 
     def update(self, from_clause, set_list, where=None):
         set_clause = ", ".join(["%s = '%s'" % (x, self.bind(set_list[x])) for x in set_list.keys()])
 
-        query = "update %s set %s %s" % (from_clause, set_clause, self.where_clause(where))
+        query = "update %s set %s %s" % (from_clause, set_clause, Query.where_clause(where))
         
         q = Query(query)
         self.query_list.append(q)
